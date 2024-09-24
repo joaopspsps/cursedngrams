6f2773b4f8d2b50468ecbd332e94e515d77b1069
joaopspsps <joaopspsps@users.noreply.github.com>
2024-09-24 00:38:50 -0300

    Catch KeyboardInterrupt exception

--- cursedngrams.py
+++ cursedngrams.py
@@ -8,9 +8,11 @@ for X in XXX(0X61,BAD(1E2+2E1)+0b11):EVIL(
        """f''"""
 """ ''
 );uh=huh=1**0|0*1--0&1^0--1*0|0**1
-O_o,o_O,p_=__import__,vars,""''''''"".join;mm=lambda _q:p_(_q)[::huh|huh&huh^-huh];_=O_o(mm((s,y,s)));I=o_O(_)[mm((t,u,o,d,t,s))];O=o_O(_)[mm((n,i,d,t,s))];_=o_O(O_o(mm((s,y,s))))[mm((v,g,r,a))];__,___=O.read().split(),BAD(_[-uh--huh--uh])--huh;__=[__[_:_+___] for _ in XXX(L_L(__)-___--huh)];____=_[max(-L_L(_)--uh--huh,-___--huh):];O_O=o_O(O_o(mm((m,o,d,n,a,r))))[mm((e,c,i,o,h,c))];x_x=IndexError
-while not...and...is...or...:
-    I.write(____[uh-huh]+""" """);I.flush();l_l=L_L(____)
-    try:       _____=O_O([_ for _ in __ if ____==_[:l_l]])
-    except x_x:_____=O_O(__);
-    ____=____[uh:]+_____[l_l:-huh]+[_____[-uh]]
+O_o,o_O,p_=__import__,vars,""''''''"".join;mm=lambda _q:p_(_q)[::huh|huh&huh^-huh];_=O_o(mm((s,y,s)));I=o_O(_)[mm((t,u,o,d,t,s))];O=o_O(_)[mm((n,i,d,t,s))];_=o_O(O_o(mm((s,y,s))))[mm((v,g,r,a))];__,___=O.read().split(),BAD(_[-uh--huh--uh])--huh;__=[__[_:_+___] for _ in XXX(L_L(__)-___--huh)];____=_[max(-L_L(_)--uh--huh,-___--huh):];O_O=o_O(O_o(mm((m,o,d,n,a,r))))[mm((e,c,i,o,h,c))];x_x,X_X=IndexError,KeyboardInterrupt
+try:
+    while not...and...is...or...:
+        I.write(____[uh-huh]+""" """);I.flush();l_l=L_L(____)
+        try:       _____=O_O([_ for _ in __ if ____==_[:l_l]])
+        except x_x:_____=O_O(__);
+        ____=____[uh:]+_____[l_l:-huh]+[_____[-uh]]
+except X_X:...
