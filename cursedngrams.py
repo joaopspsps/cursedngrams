c4b900a3441a18a90e88eb2013f9ff597fe05350
joaopspsps <joaopspsps@users.noreply.github.com>
2024-09-28 22:25:51 -0300

    😈

--- cursedngrams.py
+++ cursedngrams.py
@@ -33,7 +33,7 @@ __                           =                                 O_O.read().split(
 ___                                 =                          BAD(NO[-uh--huh--huh]);...if(___>uh)else(___:=--uh--huh)
 ____                             =                             NO[(uhhuh:=_m_m_(-_X_X_X_X_(NO)--uh--huh,-___--huh)):]if(NO[--uh--huh:])else[[_l_l_l_(__),uhhuh:=uh-huh][-huh-uh]]
 ___                           =                                ___=___=___=___=___=___=___=___=___=___=___=___
-__                                    =                        [__[X:X+___] for X in XXX(_X_X_X_X_(__)-___--huh)]
+__                                    =                        [[__[X:X+___] for X in XXX(_X_X_X_X_(__)-___--huh)]][uh^huh]
 _l_l_l_                   =                                    _l_l_l_=_l_l_l_=_l_l_l_=_l_l_l_=_l_l_l_=_l_l_l_
 p_p                                        =                   p_p=p_p=p_p=p_p=p_p=p_p=p_p=p_p=p_p=p_p=p_p=p_p
 q_q                  =                                         q_q=q_q=q_q=q_q=q_q=q_q=q_q=q_q=q_q=q_q=q_q=q_q
@@ -41,38 +41,45 @@ NO                                               =             NO=NO=NO=NO=NO=NO
 YES             =                                              YES=YES=YES=YES=YES=YES=YES=YES=YES=YES=YES=YES
 mm                                                     =       mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm
 O_o,o_O     =                                                  [o_O,O_o][uh-huh:uh--huh:huh][::---uh--huh-huh]
-if(...)and(_                                               :=  NO[uh--huh:uhhuh]):_l__l_(*_)
+if(...)and(_                                               :=  NO[uh--huh:uhhuh]):_l__l_(*(*(*(*(*(_),),),),),)
 #´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
 #` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
 #´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
 #` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
 try:#_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
-    while not...and...is...or...:#    Cursed be that as in other and more tender than the skin of the whaleboat,
-        _l__l_                  (#    and the Dragon; which dragon I maintain to have procured for every
-        ____[                   (#    calm. There is some sixteen or eighteen feet in the sand; grown bolder,
-        uh                      )#    they waded out with nets for mackerel; more experienced, they pushed
-        -                       (#    off in time. What of it, I knew not; but so intolerably cold that we
-        huh                     )#    are surrounded by circle upon circle of these Dutch whalemen, including
-        ]                       )#    the short northern day merged into night, we found ourselves sitting up;
-        ______=                 (#    the clothes well tucked around us, and we saw for a whole ship's company
-        _X_X_X_X_               (#    down to "the Moss," the little Moss tossed the still struggling ringleader
-        ____                    )#    was shoved up into the sea, alternate with their passports to quit the
-                                )#    Kingdom of Cetology. Now, then, be the signal was set apart and sanctified
-        try:_____=              (#    the whale. Soon, while the thought of being brought by the crossing and
-        _l_l_l_                 (#    re-crossing, into ten thousand fathoms beneath the ship's full middle,
-        [_ for _ in             (#    all her spars thickly clustering with men. At the same fate may be fitly
-        __                      )#    inquired, whether, in case a parcel of dark nods replied, Yes. Three
-        if                      (#    punctures were made of solid bronze, and shaped in an undiscriminating
-        ____                    )#    sweep of things at one hundred and twenty-eight feet. And Lacépède, the
-        ==                      (#    French soldiers in the sea. His heart had burst! "He's dead, Mr. Stubb,"
-        _[:______]              )#    and descends the scuttle. The second Emir lounges about the bigness of
-                                ]#    a third old gentleman in Hackluyt uses the following titles: The Whale;
-                                )#    the Quog Whale; the Quog Whale; the Right Whale has no extern nose; and
-                                )#    that done, dish it; d'ye hear?
-        except q_q:_____=       (#
-        _l_l_l_                 (#        --- Herman Melville + cursedngrams
+    while not...and...is...or...:#\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
+        _l__l_                  (#
+        ____[                   (#
+        uh                      )#
+        -                       (#
+        huh                     )#    Cursed be that as in other and more tender than the skin of the whaleboat,
+        ]                       )#    and the Dragon; which dragon I maintain to have procured for every
+        ______=______=______=   (#    calm. There is some sixteen or eighteen feet in the sand; grown bolder,
+        _X_X_X_X_               (#    they waded out with nets for mackerel; more experienced, they pushed
+        ____                    )#    off in time. What of it, I knew not; but so intolerably cold that we
+                                )#    are surrounded by circle upon circle of these Dutch whalemen, including
+        try:_____=_____=_____=  (#    the short northern day merged into night, we found ourselves sitting up;
+        _l_l_l_                 (#    the clothes well tucked around us, and we saw for a whole ship's company
+        [                       (#    down to "the Moss," the little Moss tossed the still struggling ringleader
+        _                       )#    was shoved up into the sea, alternate with their passports to quit the
+        for                     (#    Kingdom of Cetology. Now, then, be the signal was set apart and sanctified
+        _                       )#    the whale. Soon, while the thought of being brought by the crossing and
+        in                      (#    re-crossing, into ten thousand fathoms beneath the ship's full middle,
+        __                      )#    all her spars thickly clustering with men. At the same fate may be fitly
+        if                      (#    inquired, whether, in case a parcel of dark nods replied, Yes. Three
+        ____                    )#    punctures were made of solid bronze, and shaped in an undiscriminating
+        ==                      (#    sweep of things at one hundred and twenty-eight feet. And Lacépède, the
+        _                       [#    French soldiers in the sea. His heart had burst! "He's dead, Mr. Stubb,"
+        :                       (#    and descends the scuttle. The second Emir lounges about the bigness of
+        ______                  )#    a third old gentleman in Hackluyt uses the following titles: The Whale;
+        ]                       )#    the Quog Whale; the Quog Whale; the Right Whale has no extern nose; and
+                                ]#    that done, dish it; d'ye hear?
+                                )#
+                                )#        --- Herman Melville + cursedngrams
+        except q_q:_____=_____= (#
+        _l_l_l_                 (#
         __                      )#
-                                )#\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´
+                                )#
         ____=____=____=____=____=____[uh:]+_____[______:-huh]+[_____[-uh]]
 except  p_p:...#\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#`
 #` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´
