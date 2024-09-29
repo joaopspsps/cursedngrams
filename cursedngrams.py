b3254610c888bfb4549bc67956868ee405494ab2
joaopspsps <joaopspsps@users.noreply.github.com>
2024-09-28 22:02:18 -0300

    😈

--- cursedngrams.py
+++ cursedngrams.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env -S env env env env env env env env env env env env env env env python3
-uh=huh=1**0|0*1--0&1^0--1*0|0**1#//^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#`
-BAD,EVIL,XXX,X__X,X_,_X,_X_X_X_X_,evil,_l__l_,_m_m_=int,exec,range,"'",chr,(
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// `#´´#``#´´#``#´´#``#´
+#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ ´#``#´´#``#´´#``#´´#`
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// `#´´#``#´´#``#´´#``#´
+#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ ´#``#´´#``#´´#``#´´#`
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// `#´´#``#´´#``#´´#``#´
+uh=huh=1**0|0*1--0&1^0--1*0|0**1#//^\\ \\_// //^\\"""f"""//^\\ ´#``#´´#``#´´#``#´´#`
+BAD,EVIL,XXX,X__X,X_,_X,_X_X_X_X_,evil,_l__l_,_m_m_=int,exec,range,"'",chr,(#~~#~~#
  ((((((((((((((61))))))))))))))),len,eval,lambda *_:print(*_,end=""" """,flush=1),max
-for(X)in(XXX(0X61,BAD(1E2+2E1)+0b11)):EVIL("""#_//"""f"""\\_// ´#``#´´#``#´´#``#´
-#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#`
-#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´
-#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#`
-#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´
-#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#`
-#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´
-{ X_(X)}"""f"""{X_(_X)}"""f"""{X__X}"""f"""{X_(X)}"""f"""{X__X} #~~#~~#~~#~~#~~#~
-#` \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´`#´`#´`#´`#´`#`
-#´ //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#`´#`´#`´#`´#`´#´
-#` \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´`#´`#´`#´`#´`#`
-#´ //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#`´#`´#`´#`´#`´#´
-#` \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´`#´`#´`#´`#´`#`
-#´ //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#`´#`´#`´#`´#`´#´
+for(X)in(XXX(0X61,BAD(1E2+2E1)+0b11)):EVIL("""#_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
+#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
+#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
+#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
+{ X_(X)}"""f"""{X_(_X)}"""f"""{X__X}"""f"""{X_(X)}"""f"""{X__X} #~~#~~#~~#~~#~~#~~#
+#` \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
+#´ //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
+#` \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
+#´ //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
+#` \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
+#´ //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
 """);p_    =                                                   """""""""""""""""".join
-O_o,o_O                                                =       __import__,vars
+O_o,o_O                                                =       __import__,vars #``#´
 mm             =                                               lambda _q:p_(_q)[::huh|huh&huh^-huh]
 YES                                               =            O_o(mm((s,y,s)));O_O=o_O(YES)[mm((n,i,d,t,s))]
 NO                  =                                          o_O(YES)[mm((v,g,r,a))]
-q_q                                          =                 IndexError
-p_p                     =                                      KeyboardInterrupt
+q_q                                          =                 IndexError#``#´´#``#´
+p_p                     =                                      KeyboardInterrupt  #`
 _l_l_l_                                 =                      o_O(O_o(mm((m,o,d,n,a,r))))[mm((e,c,i,o,h,c))]
-__                           =                                 O_O.read().split()
+__                           =                                 O_O.read().split() #`
 ___                                 =                          BAD(NO[-uh--huh--huh]);...if(___>uh)else(___:=--uh--huh)
 ____                             =                             NO[(uhhuh:=_m_m_(-_X_X_X_X_(NO)--uh--huh,-___--huh)):]if(NO[--uh--huh:])else[[_l_l_l_(__),uhhuh:=uh-huh][-huh-uh]]
 ___                           =                                ___=___=___=___=___=___=___=___=___=___=___=___
@@ -37,32 +42,41 @@ YES             =                                              YES=YES=YES=YES=Y
 mm                                                     =       mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm=mm
 O_o,o_O     =                                                  [o_O,O_o][uh-huh:uh--huh:huh][::---uh--huh-huh]
 if(...)and(_                                               :=  NO[uh--huh:uhhuh]):_l__l_(*_)
-try:
-    while not...and...is...or...:  # Cursed be that as in other and more tender than the skin of the whaleboat,
-        _l__l_                  (  # and the Dragon; which dragon I maintain to have procured for every
-        ____[                   (  # calm. There is some sixteen or eighteen feet in the sand; grown bolder,
-        uh                      )  # they waded out with nets for mackerel; more experienced, they pushed
-        -                       (  # off in time. What of it, I knew not; but so intolerably cold that we
-        huh                     )  # are surrounded by circle upon circle of these Dutch whalemen, including
-        ]                       )  # the short northern day merged into night, we found ourselves sitting up;
-        ______=                 (  # the clothes well tucked around us, and we saw for a whole ship's company
-        _X_X_X_X_               (  # down to "the Moss," the little Moss tossed the still struggling ringleader
-        ____                    )  # was shoved up into the sea, alternate with their passports to quit the
-                                )  # Kingdom of Cetology. Now, then, be the signal was set apart and sanctified
-        try:_____=              (  # the whale. Soon, while the thought of being brought by the crossing and
-        _l_l_l_                 (  # re-crossing, into ten thousand fathoms beneath the ship's full middle,
-        [_ for _ in             (  # all her spars thickly clustering with men. At the same fate may be fitly
-        __                      )  # inquired, whether, in case a parcel of dark nods replied, Yes. Three
-        if                      (  # punctures were made of solid bronze, and shaped in an undiscriminating
-        ____                    )  # sweep of things at one hundred and twenty-eight feet. And Lacépède, the
-        ==                      (  # French soldiers in the sea. His heart had burst! "He's dead, Mr. Stubb,"
-        _[:______]              )  # and descends the scuttle. The second Emir lounges about the bigness of
-                                ]  # a third old gentleman in Hackluyt uses the following titles: The Whale;
-                                )  # the Quog Whale; the Quog Whale; the Right Whale has no extern nose; and
-                                )  # that done, dish it; d'ye hear?
-        except q_q:_____=       (  #
-        _l_l_l_                 (  #     --- Herman Melville + cursedngrams
-        __                      )  #
-                                )  #
+#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
+#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´´#`
+try:#_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#``#´
+    while not...and...is...or...:#    Cursed be that as in other and more tender than the skin of the whaleboat,
+        _l__l_                  (#    and the Dragon; which dragon I maintain to have procured for every
+        ____[                   (#    calm. There is some sixteen or eighteen feet in the sand; grown bolder,
+        uh                      )#    they waded out with nets for mackerel; more experienced, they pushed
+        -                       (#    off in time. What of it, I knew not; but so intolerably cold that we
+        huh                     )#    are surrounded by circle upon circle of these Dutch whalemen, including
+        ]                       )#    the short northern day merged into night, we found ourselves sitting up;
+        ______=                 (#    the clothes well tucked around us, and we saw for a whole ship's company
+        _X_X_X_X_               (#    down to "the Moss," the little Moss tossed the still struggling ringleader
+        ____                    )#    was shoved up into the sea, alternate with their passports to quit the
+                                )#    Kingdom of Cetology. Now, then, be the signal was set apart and sanctified
+        try:_____=              (#    the whale. Soon, while the thought of being brought by the crossing and
+        _l_l_l_                 (#    re-crossing, into ten thousand fathoms beneath the ship's full middle,
+        [_ for _ in             (#    all her spars thickly clustering with men. At the same fate may be fitly
+        __                      )#    inquired, whether, in case a parcel of dark nods replied, Yes. Three
+        if                      (#    punctures were made of solid bronze, and shaped in an undiscriminating
+        ____                    )#    sweep of things at one hundred and twenty-eight feet. And Lacépède, the
+        ==                      (#    French soldiers in the sea. His heart had burst! "He's dead, Mr. Stubb,"
+        _[:______]              )#    and descends the scuttle. The second Emir lounges about the bigness of
+                                ]#    a third old gentleman in Hackluyt uses the following titles: The Whale;
+                                )#    the Quog Whale; the Quog Whale; the Right Whale has no extern nose; and
+                                )#    that done, dish it; d'ye hear?
+        except q_q:_____=       (#
+        _l_l_l_                 (#        --- Herman Melville + cursedngrams
+        __                      )#
+                                )#\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´
         ____=____=____=____=____=____[uh:]+_____[______:-huh]+[_____[-uh]]
-except  p_p:...
+except  p_p:...#\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#`
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´
+#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#`
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´
+#` //^\\"""f"""//^\\ \\_// //^\\ \\_// //^\\ \\_//"""f"""\\_// ´#``#´´#``#´´#``#´
+#´ \\_//"""f"""\\_// //^\\ \\_// //^\\ \\_// //^\\"""f"""//^\\ `#´´#``#´´#``#´´#`
